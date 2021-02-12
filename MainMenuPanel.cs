7f470e35905cdf712f5162d91bba1a0410e1f938
scott <scott@LAPTOP-1K5G3HM6>
2021-02-12 02:52:20 +0000

    Added a help message box with the rules and a button called help

--- MainMenuPanel.cs
+++ MainMenuPanel.cs
@@ -31,10 +31,19 @@ namespace GridGame
             playButton.Click += new EventHandler((sender, e) => parentForm.PreGamePanel.SwitchTo());
             Controls.Add(playButton);
 
+            var helpButton = new Button
+            {
+                Text = "Help",
+                Location = new Point(playButton.Location.X, playButton.Location.Y + playButton.Height),
+                Size = new Size(BUTTON_WIDTH, BUTTON_HEIGHT),
+            };
+            helpButton.Click += new EventHandler((sender, e) => parentForm.MainMenuPanel.Help());
+            Controls.Add(helpButton);
+
             var quitButton = new Button
             {
                 Text = "Quit",
-                Location = new Point(playButton.Location.X, playButton.Location.Y + playButton.Height),
+                Location = new Point(helpButton.Location.X, helpButton.Location.Y + helpButton.Height),
                 Size = new Size(BUTTON_WIDTH, BUTTON_HEIGHT),
             };
             quitButton.Click += new EventHandler((sender, e) => Application.Exit());
@@ -46,6 +55,17 @@ namespace GridGame
             parentForm.ActivePanel.Visible = false;
             parentForm.ActivePanel = this;
             parentForm.ActivePanel.Visible = true;
+        }
+
+        public void Help()
+        {
+
+            string message = "The connect 4 game rules are pretty straightforward. The game name says it, what you have to do “connect four” to win the game. After choosing the color of your choice, you have to make a row of four checkers of the same color. The rows can be done vertically, horizontally or diagonally." + Environment.NewLine
+                               + Environment.NewLine + "Each turn, the player will get the chance to drop and make the row of his checkers, but sometimes instead of making your row, the player has to stop another opponent from making his 4 checkers row by dropping your color checker in that place." + Environment.NewLine
+                               + Environment.NewLine + "The game ends when any players make four checkers of his color in a row, or when all the 42 slots are occupied, in that case, the game ends in a tie.";
+            string title = "Rules";
+            MessageBox.Show(message, title);
+
         }
     }
 }
