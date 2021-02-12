ffae5808a032f49dd249ceb32a98fa5582c9490d
scott <scott@LAPTOP-1K5G3HM6>
2021-02-12 12:06:05 +0000

    Help box done, scoring system attempted

--- VictoryPanel.cs
+++ VictoryPanel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace GridGame
 {
@@ -15,6 +17,15 @@ namespace GridGame
         private static readonly int BUTTON_WIDTH = 200;
         private static readonly int BUTTON_HEIGHT = 50;
 
+        [Serializable()]
+        public class HighScore
+        {
+            public int Score { get; set; }
+            public string Initials { get; set; }
+        }
+
+        public List<HighScore> _highScores = new List<HighScore>();
+
         public VictoryPanel(GameForm parentForm)
         {
             this.parentForm = parentForm;
@@ -22,6 +33,15 @@ namespace GridGame
             Location = new Point(0, 0);
             Size = new Size(parentForm.Width, parentForm.Height);
             Name = "VictoryPanel";
+
+            var score = new HighScore() { Score = 100, Initials = "MAJ" };
+            _highScores.Add(score);
+            var serializer = new XmlSerializer(_highScores.GetType(), "HighScores.Scores");
+            using (var writer = new StreamWriter("highscores.xml", false))
+            {
+                serializer.Serialize(writer.BaseStream, _highScores);
+            }
+
 
             var playAgainButton = new Button();
             playAgainButton.SetBounds((Width / 2) - BUTTON_WIDTH, Height - 100, BUTTON_WIDTH, BUTTON_HEIGHT);
@@ -33,8 +53,18 @@ namespace GridGame
             mainMenuButton.SetBounds(Width / 2, Height - 100, BUTTON_WIDTH, BUTTON_HEIGHT);
             mainMenuButton.Text = "Main Menu";
             mainMenuButton.Click += new EventHandler((sender, e) => parentForm.MainMenuPanel.SwitchTo());
-            
             Controls.Add(mainMenuButton);
+
+            var showScoreButton = new Button();
+            showScoreButton.SetBounds(Width / 2, Height - 100, BUTTON_WIDTH, BUTTON_HEIGHT);
+            showScoreButton.Text = "Show Score";
+            showScoreButton.Click += new EventHandler((sender, e) => parentForm.MainMenuPanel.SwitchTo());
+            Controls.Add(showScoreButton);
+        }
+
+        public void score()
+        {
+            = "Your Score is " + HighScore.Score;
         }
 
         public void SwitchTo()
