9f01607e40a8a2ff1a1d232292abb3810bed6c85
ScottyDog5 <55397552+ScottyDog5@users.noreply.github.com>
2021-02-12 14:47:25 +0000

    Update VictoryPanel.cs
    Deleted useless code

--- VictoryPanel.cs
+++ VictoryPanel.cs
@@ -17,15 +17,6 @@ namespace GridGame
         private static readonly int BUTTON_WIDTH = 200;
         private static readonly int BUTTON_HEIGHT = 50;
 
-        [Serializable()]
-        public class HighScore
-        {
-            public int Score { get; set; }
-            public string Initials { get; set; }
-        }
-
-        public List<HighScore> _highScores = new List<HighScore>();
-
         public VictoryPanel(GameForm parentForm)
         {
             this.parentForm = parentForm;
@@ -33,15 +24,6 @@ namespace GridGame
             Location = new Point(0, 0);
             Size = new Size(parentForm.Width, parentForm.Height);
             Name = "VictoryPanel";
-
-            var score = new HighScore() { Score = 100, Initials = "MAJ" };
-            _highScores.Add(score);
-            var serializer = new XmlSerializer(_highScores.GetType(), "HighScores.Scores");
-            using (var writer = new StreamWriter("highscores.xml", false))
-            {
-                serializer.Serialize(writer.BaseStream, _highScores);
-            }
-
 
             var playAgainButton = new Button();
             playAgainButton.SetBounds((Width / 2) - BUTTON_WIDTH, Height - 100, BUTTON_WIDTH, BUTTON_HEIGHT);
@@ -54,12 +36,6 @@ namespace GridGame
             mainMenuButton.Text = "Main Menu";
             mainMenuButton.Click += new EventHandler((sender, e) => parentForm.MainMenuPanel.SwitchTo());
             Controls.Add(mainMenuButton);
-
-            var showScoreButton = new Button();
-            showScoreButton.SetBounds(Width / 2, Height - 100, BUTTON_WIDTH, BUTTON_HEIGHT);
-            showScoreButton.Text = "Show Score";
-            showScoreButton.Click += new EventHandler((sender, e) => parentForm.MainMenuPanel.SwitchTo());
-            Controls.Add(showScoreButton);
         }
 
         public void SwitchTo()
